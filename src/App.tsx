88be2a7d11ddf33e08c5cc12fadb654c525030f2
lakshya820 <lakshyaanand820@gmail.com>
2024-07-16 16:30:01 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import BarChart1 from './Pages/BarChart1'
+
+function App() {
+ // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BarChart1></BarChart1>
+    </>
+  )
+}
+
+export default App
